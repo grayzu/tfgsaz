a378d6a928ad078fb119f7e6a685add1f6ac01d2
Glenn Minch <glenn.minch@live.com>
2019-10-07 22:06:19 -0700

    misc changes

--- code/4-variables/main.tf
+++ code/4-variables/main.tf
@@ -94,7 +94,7 @@ resource "azurerm_virtual_machine" "vm" {
     location              = var.location
     resource_group_name   = azurerm_resource_group.rg.name
     network_interface_ids = [azurerm_network_interface.nic.id]
-    vm_size               = "Standard_DS1_v2"
+    vm_size               = "Standard_B1s"
     tags                  = var.tags
 
     storage_os_disk {
